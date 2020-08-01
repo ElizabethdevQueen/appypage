f40a875ac0d5186c547bbbdbf18689a595903327
Elizabeth Segun-Ajiboye <elizabethaderemi@gmail.com>
2020-08-01 15:39:03 +0100

    working on faq section

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react';
 import './Features.css';
+import Faqaccordion from './Faqaccordion';
 
 class Faq extends Component {
+    state = {
+        data:[
+            {
+                title:"Faq first question goes here?",
+                text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            },
+            {
+                title: "About frequent question goes here?",
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            },
+            {
+                title: "Why more question goes here?",
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            },
+            {
+                title: "What question goes here?",
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            }
+        ],
+        data2:[
+            {
+                title: "Faq second question goes here?",
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            },
+            {
+                title: "Third faq question goes here?",
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            },
+            {
+                title: "Why more question goes here?",
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            },
+            {
+                title: "Seventh frequent question here?",
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            }
+        ]
+    }
     render() {
+        const{data, data2} = this.state;
         return (
             <div className="faq-container">
                 <div className="featuretext">
@@ -10,8 +50,22 @@ class Faq extends Component {
                     <h3>Frequently Asked Questions</h3>
                 </div>
 
-                <div>
+                <div className="faq-content">
+                    <div className="fcontent">
+                        <div className="faq-accordion">
+                            {data.map((item,index)=>{
+                                return <Faqaccordion Key={index} title={item.title} text={item.text} />
+                                })
+                                }
+                        </div>
                     
+                        <div className="faq-accordion">
+                            {data2.map((item, index) => {
+                                return <Faqaccordion Key={index} title={item.title} text={item.text} />
+                            })
+                            }
+                        </div>
+                    </div>
                 </div>
             </div>
         );
---
+++ src/components/Faqaccordion.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './Features.css';
+
+class Faqaccordion extends Component {
+    state={
+        showInfo:false
+    }
+    handleToggle=()=>{
+        this.setState({
+            showInfo:this.state.showInfo
+        })
+    }
+    render(props) {
+        const {title,text}= this.props;
+        return (
+            <section className="faqsection">
+                <div className="single-accord">
+                    <div className={this.state.showInfo?'tab active':'tab'} onClick={this.handleToggle}>
+                        <i class="fa fa-plus" aria-hidden="true" onClick={this.handleToggle}></i> <p>{title}</p>
+                    </div>
+                    <div className={this.state.showInfo?'showContent content': 'content'}>
+                        <p>{text}</p>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Faqaccordion;
